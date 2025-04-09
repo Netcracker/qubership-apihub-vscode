26fdd5318a12211b1f0789aafebe3c5bcf7f2f63
Pavel Makeev <pavel.makeev.qubership@gmail.com>
2025-04-09 17:45:51 +0400

    test: Fake commit

--- src/ui-test/utils/common.utils.ts
+++ src/ui-test/utils/common.utils.ts
@@ -1,3 +1,3 @@
-export const delay = (ms: number): Promise<void> => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+export const delay = (milliseconds: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
